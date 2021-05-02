44e017e35afbcc0a3e2cbc0d8b52808bd810a3dd
mtsguim <mtsguim@gmail.com>
2021-05-02 12:30:50 -0300

    mudar para dados locais

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,6 +1,6 @@
 import { format, parseISO } from 'date-fns'
 import next, { GetStaticPaths, GetStaticProps } from 'next'
-import { api } from '../../services/api'
+// import { api } from '../../services/api'
 import { convertDurationToTimeString } from '../../utils/convertDuration'
 import ptBR from 'date-fns/locale/pt-BR'
 import styles from '../episodes/episode.module.scss'
@@ -9,6 +9,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { usePlayer } from '../../store/PlayerContext'
 import Head from 'next/head'
+import { episodesGroup } from '../../constants/episodesGroup'
 
 type Episode = {
     id: string
@@ -77,15 +78,15 @@ export default function Episode({ episode }: EpisodeProps) {
 // client (Browser) -> next.js (node.js) -> server (back-end)
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const { data } = await api.get('episodes', {
-        params: {
-            _limit: 2,
-            _sort: 'published_at',
-            _order: 'desc'
-        }
-    })
+    // const { data } = await api.get('episodes', {
+    //     params: {
+    //         _limit: 2,
+    //         _sort: 'published_at',
+    //         _order: 'desc'
+    //     }
+    // })
 
-    const paths = data.map(episode => {
+    const paths = episodesGroup.map(episode => {
         return {
             params: {
                 slug: episode.id
@@ -132,8 +133,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
     const { slug } = ctx.params
-    const { data } = await api.get(`/episodes/${slug}`)
-
+    // const { data } = await api.get(`/episodes/${slug}`)
+    const data = episodesGroup.find( e => e.id === slug) 
     const episode = {
         id: data.id,
         title: data.title,
